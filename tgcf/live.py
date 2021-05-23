0bfd1ff162f4a59af29c56a830961894ae74e6ee
SLExerciseBook <73883630+SLExerciseBook@users.noreply.github.com>
2021-05-23 15:03:16 +0530

    Update live.py

--- tgcf/live.py
+++ tgcf/live.py
@@ -40,7 +40,7 @@ async def new_message_handler(event):
     if chat_id not in config.from_to:
         return
     logging.info(f"New message received in {chat_id}")
-    message = event.message + event.chat_id
+    message = event.message
     
     global _stored  # pylint: disable=global-statement,invalid-name
 
